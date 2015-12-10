908fcc422fae2095e3df2cdaa63f641cc67ba9c8
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-12-10 01:57:51 -0600

    Add and customize devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'home#index', as: :home
+    end
+
+    unauthenticated :user do
+      root to: 'devise/sessions#new'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
