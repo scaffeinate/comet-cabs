5ed913061f863ba0eb56e12b22a98b3f9e5bf37f
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-12-18 04:15:01 -0600

    Password flow, card completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
   before_action :set_user
-  before_action :authenticate_user!
 
   def save_password
+    if @user.update(password: params[:password], password_confirmation: params[:password_confirmation], phone: params[:phone])
+      @user.is_password_set = true
+      @user.save
+      redirect_to set_card_info_user_path(@user)
+    else
+      flash[:error] = @user.errors.full_messages.first
+      redirect_to set_password_user_path(@user)
+    end
   end
 
   def save_card_info
     @payment = Payment.new(holder_name: params[:holder_name], card_number: params[:card_number],
                            card_cvv: params[:card_cvv], card_expiry: params[:card_expiry], user: @user)
 
-    if @payment.save!
-      @user.update(is_card_info_set: true)
-      redirect_to root_path
+    if @payment.save
+      @user.is_card_info_set = true
+      if @user.save
+        sign_in(@user)
+        redirect_to root_path
+      end
     else
       flash[:error] = @payment.errors.full_messages.first
       redirect_to set_card_info_user_path(@user)
