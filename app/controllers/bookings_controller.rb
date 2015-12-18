7b7a4bb670ec7f5f4fcbd57916da748ce4a2d9fe
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-12-18 10:38:01 -0600

    Booking a cab, finding drivers done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   include FareEstimate
+  include FindDriver
 
   def new
     @cab_types = CabType.all
@@ -10,16 +11,31 @@ class BookingsController < ApplicationController
     destination = params[:destination_location]
     cab_type = CabType.find(params[:cab_type])
 
-    distance = calculate_distance(params[:source].split(','), params[:destination].split(',')).round(2)
-    total = calculate_fare_estimate(distance, cab_type).round(2) + calculate_tax(fare).round(2)
+    distance = calculate_distance(source.split(','), source.split(',')).round(2)
+    fare = calculate_fare_estimate(distance, cab_type).round(2)
+    tax = calculate_tax(fare).round(2)
+    total = (fare + tax)
 
     @driver = find_driver(source, cab_type)
-    @booking = Booking.create(source: source, destination: destination, distance: distance, fare: total, user: current_user, driver: @driver, status: 1)
-    session[:current_booking] = @booking.id
+    @booking = Booking.new(source: source, destination: destination, distance: distance, price: total, user: current_user, driver: @driver, status: 1, cab_type: cab_type)
+
+    if @booking.save
+      @driver.update(available: false)
+      session[:current_booking] = @booking.id
+    else
+      @error = @driver.errors.full_messages.fist
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def confirm_booking
     @booking = Booking.find(session[:current_booking])
     @booking.update(status: 2)
+  end
+
+  def current_booking
   end
 end
--- app/controllers/concerns/find_driver.rb
+++ app/controllers/concerns/find_driver.rb
@@ -1,6 +1,28 @@
 module FindDriver
   extend ActiveSupport::Concern
   def find_driver(_source, _cab_type)
-    # code
+    lat = _source.split(',')[0].to_f
+    lng = _source.split(',')[1].to_f
+
+    records = Driver.find_by_sql ["SELECT id, (3959 * acos (
+      cos(radians(?))
+      * cos(radians(latitude))
+      * cos(radians(longitude) - radians(?))
+      + sin(radians(?))
+      * sin(radians(latitude))
+      )
+    ) AS distance FROM Drivers ORDER BY (3959 * acos (
+      cos(radians(?))
+      * cos(radians(latitude))
+      * cos(radians(longitude) - radians(?))
+      + sin(radians(?))
+      * sin(radians(latitude))
+      )
+    ) LIMIT 10", lat, lng, lat, lat, lng, lat]
+
+    records.each do |_r|
+      driver = Driver.find(_r)
+      return driver if driver.available?
+    end
   end
 end
