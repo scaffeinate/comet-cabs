c52671248ab8adb85799f008e7916cc27c24f97c
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-12-17 20:59:04 -0600

    Bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,14 @@ class BookingsController < ApplicationController
   def new
     @cab_types = CabType.all
   end
+
+  def find_cabs
+    @source = params[:source_location]
+  end
+
+  def create
+    @source = params[:source_location]
+    @destination = params[:destination_location]
+    @cab_type = params[:cab_type]
+  end
 end
